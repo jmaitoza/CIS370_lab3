8d7ab501590f29d651f7c686ddc15936e13beaba
Joshua Maitoza <jmaitoza@umassd.edu>
2022-02-09 08:07:02 -0500

    fixed part2

--- lab3/MyCommand2.c
+++ lab3/MyCommand2.c
@@ -3,29 +3,26 @@
 #define MAX_NUM_OF_ARGS 5
 #define MAX_ARG_SIZE 256
 
+int main(int argc, char **argv) {
+  char argumentArray[MAX_NUM_OF_ARGS][MAX_ARG_SIZE] = {
+      {'d', 'e', 'f', 'a', 'u', 'l', 't', 'A', 'r', 'g'},
+      {'d', 'e', 'f', 'a', 'u', 'l', 't', 'A', 'r', 'g'},
+      {'d', 'e', 'f', 'a', 'u', 'l', 't', 'A', 'r', 'g'},
+      {'d', 'e', 'f', 'a', 'u', 'l', 't', 'A', 'r', 'g'},
+      {'d', 'e', 'f', 'a', 'u', 'l', 't', 'A', 'r', 'g'}};
 
-int main (int argc, char **argv)
-{
-  char argumentArray[MAX_NUM_OF_ARGS][MAX_ARG_SIZE] = 
-  {
-    {'d','e','f','a','u','l','t','A','r','g'},
-    {'d','e','f','a','u','l','t','A','r','g'},
-    {'d','e','f','a','u','l','t','A','r','g'},
-    {'d','e','f','a','u','l','t','A','r','g'}
-  };
-
-  // for (int j = 0; j < 5; j++)
-  // {
-  //   strcpy(argumentArray[j], "defaultArg");  
-  //     printf("%s\n", argumentArray[j]);
-  // }
-
-  for (int i = 0; i < MAX_NUM_OF_ARGS; i++)
-  {    
-     strcpy(argumentArray[i], argv[i]); 
-    // argumentArray[i] = argv[i];
-    printf("argumentArray[%d]: %s\n", i, argumentArray[i]);
+  if (argc < 6) {
+    for (int i = 0; i < argc; i++) {
+      strcpy(argumentArray[i], argv[i]);
+    }
+    for (int i = 0; i < MAX_NUM_OF_ARGS; i++) {
+      printf("argumentArray[%d]: %s\n", i, argumentArray[i]);
+    }
   }
+
+  else {
+    printf("Your arguments exceeded the maximum number of arguments (4)\n");
+  }
+
   return 0;
 }
-
